bce0d59609728bf8a7128b6d96a100c3aa3f72e8
Gusvananda Imanullah <gusvananda@gmail.com>
2019-07-04 12:17:08 +0700

    Update mcbot.js

--- mcbot.js
+++ mcbot.js
@@ -1,6 +1,28 @@
+///////////////////////////////////////////////
+//                                           //
+//         Minecraft Server Checker          //
+//                Discord Bot                //
+//       (github.com/rendrop/mcstatus)       //
+//                                           //
+//                 7/4/2019                  //
+//              Private Release              //
+//                                           //
+//          copyright: rendrop @2019         //
+//                                           //
+///////////////////////////////////////////////
+//                                           //
+//                   Using:                  //
+//                                           //
+//                McAPI.us API               //
+//                 discord.js                //
+//                  request                  //
+//                                           //
+///////////////////////////////////////////////
+
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var statustring = "No signal";
+// var statustring = "No signal";
 
 var request = require('request');
 var mcIP = process.env.mcip; // Your MC server IP
@@ -8,38 +30,8 @@ var mcPort = process.env.mcport; // Your MC server port
 var prefix = process.env.prefix;
 var url = 'http://mcapi.us/server/query?ip=' + mcIP + '&port=' + mcPort;
 var status;
-var statusID;
 var isChecking = false;
 var interval;
-
-function start(){
-	if(!isChecking){
-		isChecking = true;
-		update();
-		interval = setInterval(update, 10000);
-	}
-}
-
-function playerList(){
-	request(url, function(err, response, body) {
-		  if(err) {
-			  console.log(err)
-			  .catch(console.error);
-			  return message.reply('Error getting Minecraft server status...');
-		  }
-		  body = JSON.parse(body);
-			if(body.online) {
-				players = body.players.list;
-				length = (body.players.list).length;
-				console.log(players);
-				console.log(length);
-				client.user
-					.send(message)
-						.then(message => console.log(`${message.content}`))
-						.catch(console.error);
-			}
-	});
-}
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -47,24 +39,22 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
-  if(message.author.bot) return;
-  
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
-  if(message.content.indexOf(prefix) !== 0) return;
-  
-  // Here we separate our "command" name, and our "arguments" for the command. 
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  // Let's go with a few common example commands! Feel free to delete or change those.
+	// This event will run on every single message received, from any channel or DM.
+	  
+	// It's good practice to ignore other bots. This also makes your bot ignore itself
+	// and not get into a spam loop (we call that "botception").
+	if(message.author.bot) return;
+	  
+	// Also good practice to ignore any message that does not start with our prefix, 
+	// which is set in the server configuration.
+	if(message.content.indexOf(prefix) !== 0) return;
+	 
+	// Here we separate our "command" name, and our "arguments" for the command. 
+	// e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+	// command = say
+	// args = ["Is", "this", "the", "real", "life?"]
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
   
 	if(command === "status") {
 		message.delete().catch(O_o=>{});
@@ -120,25 +110,73 @@ client.on("message", async message => {
 					}
 				});
 			},5000);
+		} else {
+			m.edit("There is a check on process");
 		}
 	}
 
 	if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
-    
     // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
+	const deleteCount = parseInt(args[0], 10);
     
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
+		// Combined conditions. <3
+		if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+			return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+		
+		// So we get our messages, and delete them. Simple enough.
+		const fetched = await message.channel.fetchMessages({limit: deleteCount});
+			message.channel.bulkDelete(fetched)
+			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+	}
 
+	if(command === "player") {
+		message.delete().catch(O_o=>{});
+		const p = await message.channel.send("Checking online players.");
+		const d = await message.channel.send("This feature is still on development.");
+		setTimeout(function(){
+			request(url, function(err, response, body) {
+					if(err) {
+					console.log(err)
+					.catch(console.error);
+					return message.reply('Error getting Minecraft server status...');
+				}
+			body = JSON.parse(body);
+				if(body.online) {
+					players = body.players.list;
+					length = (body.players.list).length;
+					console.log(players);
+					console.log(length);
+					if(length > 0){
+						p.edit(`Players: ` + players);
+					} else {
+						p.edit("There are no players online");
+				} else {
+					p.edit("Error getting player list. The server seems to be offline. :(");
+				}
+				});
+			},2000);
+		}
+	}
+	
+	if(command === "stop") {
+		
+		message.delete().catch(O_o=>{});
+		const s = await message.channel.send("Stopping server check");
+		
+		if(isChecking){
+			setTimeout(function(){
+				s.edit("Check stopped");
+				client.user.setActivity("Type +status to check server status", { type: 'PLAYING' })
+			},2000);
+		} else {
+			s.edit("There is no check ongoing");
+	}
+	
+	if(command === "help") {
+		message.delete().catch(O_o={});
+		const h = await message.channel.send("Help menu is still under development");
+	}	
 });
 
 client.login(process.env.token);
