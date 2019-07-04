d28673c0533894926d7f6a4605dc25a36814f030
Gusvananda Imanullah <gusvananda@gmail.com>
2019-07-04 15:26:53 +0700

    Update mcbot.js

--- mcbot.js
+++ mcbot.js
@@ -22,12 +22,10 @@
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-// var statustring = "No signal";
-
 var request = require('request');
-var mcIP = process.env.mcip; // Your MC server IP
-var mcPort = process.env.mcport; // Your MC server port
-var prefix = process.env.prefix;
+var mcIP = process.env.mcip; // Read var "mcip" from Heroku ENV
+var mcPort = process.env.mcport; // Read var "mcport" from Heroku ENV
+var prefix = process.env.prefix; // Read var "prefix" from Heroku ENV
 var url = 'http://mcapi.us/server/query?ip=' + mcIP + '&port=' + mcPort;
 var status;
 var isChecking = false;
@@ -55,16 +53,21 @@ client.on("message", async message => {
 	// args = ["Is", "this", "the", "real", "life?"]
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
-  
+	
+// status command: get minecraft server status
+	
 	if(command === "status") {
 		message.delete().catch(O_o=>{});
-		// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-		// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+		// Send confirmation message
 		const m = await message.channel.send("Checking server status...");
+		// Check if the routine is already running or not
 		if(!isChecking){
+			// Routine is running
 			isChecking = true;
-			console.log("Update called");
+			// console.log("Update called");
+			// Set bot status
 			client.user.setActivity("Checking server status...", { type: 'PLAYING' });
+			// Routine, check server status every 5 seconds
 			interval = setInterval(function(){
 				request(url, function(err, response, body) {
 					if(err) {
@@ -79,26 +82,37 @@ client.on("message", async message => {
 							client.user.setStatus('dnd')
 							//.then(console.log)
 							.catch(console.error);
+							// Server is offline
+							// Edit confirmation message
 							m.edit(`Server is offline :(`);
+							// Edit bot status
 							client.user.setActivity("Server offline", { type: 'PLAYING' });
+							// Stop the routine
 							isChecking = false;
 							clearInterval(interval);
-							console.log("Server offline");
+							// console.log("Server offline");
 						  }else{
 							client.user.setStatus('online')
 							//.then(console.log)
 							.catch(console.error);
 						//console.log("Number of player: " + ((body.players.list).counters.length));
-							console.log("Server online");
+							// console.log("Server online");
 						}
+						// Read the amount of player
 						if(body.players.now) {
+							// There are player(s) in the server
+							// Set bot status
 							status = ' ' + body.players.now + '  of  ' + body.players.max;
 							client.user.setActivity(status);
+							// Edit the confirmation message to show the amount of online player
 							m.edit("Server is online! With " + body.players.now + " player(s) currently online.")
 						} else {
+							// There are no players in the server
+							// Set bot status
 							status = ' 0  of  ' + body.players.max;
 							client.user.setActivity(status);
-							m.edit(`Server is online! But it seems empty :(`)
+							// Edit the confirmation message
+							m.edit(`Server is **online**! But it seems empty :(`)
 						}
 					} else {
 						client.user.setStatus('dnd')
@@ -106,15 +120,19 @@ client.on("message", async message => {
 						.catch(console.error);
 						isChecking = false;
 						clearInterval(interval);
-						m.edit(`Server is offline / there is an API error or lag`);
+						m.edit(`Server is **offline** / there is an API error or lag`);
 						client.user.setActivity("Server offline / API error :(", { type: 'PLAYING' })
 					}
 				});
 			},5000);
 		} else {
-			m.edit("There is a check on process");
+			// Routine is already running
+			// Send error message
+			m.edit("**Error:** The check is already running");
 		}
 	}
+
+// purge command: (hidden from the list)
 
 	if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
@@ -131,9 +149,14 @@ client.on("message", async message => {
 			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 	}
 
+// player command: get the list of online players in server 
+// (if the server check routine is running)
+
 	if(command === "player") {
 		message.delete().catch(O_o=>{});
+		// Check if the routine is running
 		if(isChecking){
+			// Send confirmation message
 			const p = await message.channel.send("Checking online players...");
 			// const d = await message.channel.send("This feature is still on development.");
 			setTimeout(function(){
@@ -141,11 +164,16 @@ client.on("message", async message => {
 						if(err) {
 						console.log(err)
 						.catch(console.error);
+						// Reply an error message
 						return message.reply('Error getting Minecraft server status... (McAPI is down?)');
 					}
+				// Get data from McAPI	
 				body = JSON.parse(body);
+					// Server online?
 					if(body.online) {
+						// Check player list, is there any data in it?
 						if(body.players.list != null){
+							// There's data, parsing the list
 							players = body.players.list;
 							length = (body.players.list).length;
 								var i;
@@ -157,37 +185,52 @@ client.on("message", async message => {
 							// console.log(players);
 							// console.log(length);
 							if(length > 0){
+								// Edit the message
+								// Show list of online players
 								p.edit(`**Player online:** ` + playerList);
 							}
 						} else {
+							// There's no data, don't parse the list
+							// Edit confirmation message
 							p.edit("There are no players online :(");
 						}
 					} else {
+						// This is a rare error message.
 						p.edit("Error getting player list. The server seems to be offline :(");
 					}
 				});
 			},2000);
 		} else {
+			// Routine is not running, send error message
 			const e = await message.channel.send("Please check the server status first!");
 		}
 	}
-	
+
+// stop command: stop the server check routine
+
 	if(command === "stop") {
 		
 		message.delete().catch(O_o=>{});
+		// Send a confirmation message
 		const s = await message.channel.send("Stopping server check");
-		
+		// Check if the routine is running
 		if(isChecking){
+			// If there routine is running, stop it in ~2 seconds
 			setTimeout(function(){
 				clearInterval(interval);
+				// Reset the bot
 				isChecking = false;
-				s.edit("Check stopped");
 				client.user.setActivity("Type -help", { type: 'PLAYING' })
+				// Edit the message
+				s.edit("Check stopped");
 			},2000);
 		} else {
+			// Routine is not running, edit the message
 			s.edit("There is no check ongoing");
 		}
 	}
+
+// help command: show help message
 	
 	if(command === "help") {
 		message.delete().catch(O_o={});
@@ -195,6 +238,9 @@ client.on("message", async message => {
 	}	
 });
 
+// Reset the bot automatically if the checking routine stopped.
+// This can be caused by server going offline, API downtime, etc.
+// or the command stop
 if(!isChecking){
 	timeout = setTimeout(function(){
 	client.user.setActivity("Type -help", { type: 'PLAYING' })
@@ -202,372 +248,5 @@ if(!isChecking){
 }
 
 //END OF MINECRAFT SERVER CHECK
-//
-//
-// SPOTIFY
-//
-// Copyright 2018 Campbell Crowley. All rights reserved.
-// Author: Campbell Crowley (dev@campbellcrowley.com)
-const https = require('https');
-require('./subModule.js').extend(Spotify);
-/**
- * @classdesc Attempts to play what a user is playing on Spotify, to a voice
- * channel.
- * @class
- * @augments SubModule
- * @listens Command#spotify
- */
-function Spotify() {
-  const self = this;
-  this.myName = 'Spotify';
 
-  let music;
-
-  /**
-   * The request to send to spotify to fetch the currently playing information
-   * for a user.
-   *
-   * @private
-   * @default
-   * @constant
-   * @type {object}
-   */
-  const apiRequest = {
-    protocol: 'https:',
-    host: 'api.spotify.com',
-    path: '/v1/me/player/currently-playing',
-    method: 'GET',
-    headers: {
-      'User-Agent': require('./common.js').ua,
-    },
-  };
-
-  /** @inheritdoc */
-  this.initialize = function() {
-    self.command.on('spotify', commandSpotify, true);
-    checkMusic();
-  };
-  /** @inheritdoc */
-  this.shutdown = function() {
-    self.command.deleteEvent('spotify');
-    for (const i in following) {
-      if (following[i]) endFollow({guild: {id: i}});
-    }
-  };
-  /** @inheritdoc */
-  this.unloadable = function() {
-    return true;
-  };
-
-  /**
-   * The current users we are monitoring the spotify status of, and some related
-   * information. Mapped by guild id.
-   *
-   * @private
-   * @type {object}
-   */
-  const following = {};
-
-  /**
-   * Lookup what a user is listening to on Spotify, then attempt to play the
-   * song in the requester's voice channel.
-   *
-   * @private
-   * @type {commandHandler}
-   * @param {Discord~Message} msg The message that triggered command.
-   * @listens Command#spotify
-   */
-  function commandSpotify(msg) {
-    if (!self.bot.accounts) {
-      self.common.reply(msg, 'Unable to lookup account information.');
-      return;
-    }
-    let userId = msg.author.id;
-    if (msg.mentions.users.size > 0) {
-      userId = msg.mentions.users.first().id;
-    }
-    const subCmd = msg.text.trim().split(' ')[0];
-    let infoOnly = false;
-    switch (subCmd) {
-      case 'info':
-      case 'inf':
-      case 'playing':
-      case 'current':
-      case 'currently':
-      case 'status':
-      case 'stats':
-      case 'stat':
-        infoOnly = true;
-        break;
-    }
-    if (!infoOnly && music.isSubjugated(msg) && following[msg.guild.id]) {
-      endFollow(msg);
-      self.common.reply(msg, 'Stopped following Spotify.', '<@' + userId + '>');
-      if (following[msg.guild.id] && userId == following[msg.guild.id].user) {
-        return;
-      }
-    }
-    getCurrentSong(userId, (err, song) => {
-      if (err) {
-        if (err == 'Unlinked') {
-          self.common.reply(
-              msg,
-              'Discord account is not linked to Spotify.\nPlease link account' +
-                  ' at spikeybot.com to use this command.',
-              'https://www.spikeybot.com/account/');
-        } else if (err == 'Bad Response') {
-          self.common.reply(
-              msg, 'Unable to get current Spotify status.',
-              'Bad response from Spotify');
-        } else if (err == 'Nothing Playing') {
-          self.common.reply(msg, 'Not listening to anything on Spotify.');
-        } else {
-          self.common.reply(msg, 'Unable to get current Spotify status.', err);
-        }
-        if (infoOnly || err != 'Nothing Playing') return;
-      }
-      if (infoOnly) {
-        self.common.reply(
-            msg, 'Song: ' + song.name + '\nArtist: ' + song.artist +
-                '\nAlbum: ' + song.album + '\nProgress: ' +
-                Math.round(song.progress / 1000) + ' seconds in.\nCurrently ' +
-                (song.isPlaying ? 'playing' : 'paused'));
-      } else {
-        self.common.reply(
-            msg,
-            'Following Spotify music. Music control is now subjugated by ' +
-                'Spotify.\n(Please wait, seeking may take a while)',
-            '<@' + userId + '>');
-        updateFollowingState(msg, userId, song, true);
-        return;
-      }
-    });
-  }
-
-  /**
-   * Fetch the current playing song on spotify for the given discord user id.
-   *
-   * @private
-   * @param {string|number} userId The Discord user id to lookup.
-   * @param {Fucntion} cb Callback with err, and data parameters.
-   */
-  function getCurrentSong(userId, cb) {
-    self.bot.accounts.getSpotifyToken(userId, (res) => {
-      if (!res) {
-        cb('Unlinked');
-        return;
-      }
-      const req = https.request(apiRequest, (res) => {
-        let content = '';
-        res.on('data', (chunk) => {
-          content += chunk;
-        });
-        res.on('end', () => {
-          if (res.statusCode == 200) {
-            let parsed;
-            try {
-              parsed = JSON.parse(content);
-            } catch (err) {
-              self.error('Failed to parse Spotify response: ' + userId);
-              console.log(err, content);
-              cb('Bad Response');
-              return;
-            }
-            if (!parsed.item) {
-              cb('Nothing Playing');
-              return;
-            }
-            const artists = (parsed.item.artists || [])
-                .map((a) => {
-                  return a.name;
-                })
-                .join(', ');
-            const songInfo = {
-              name: parsed.item.name,
-              artist: artists,
-              album: parsed.item.album.name,
-              progress: parsed.progress_ms,
-              isPlaying: parsed.is_playing,
-              duration: parsed.duration_ms,
-            };
-            cb(null, songInfo);
-          } else if (res.statusCode == 204) {
-            cb('Nothing Playing');
-          } else {
-            self.error(
-                'Unable to fetch spotify currently playing info for user: ' +
-                userId);
-            console.error(content);
-            cb(res.statusCode || 'VERY SCARY ERROR');
-          }
-        });
-      });
-      req.setHeader('Authorization', 'Bearer ' + res);
-      req.end();
-    });
-  }
-
-  /**
-   * Check on the user's follow state and update the playing status to match.
-   *
-   * @private
-   *
-   * @param {Discord~Message} msg The message to use as context.
-   * @param {string|number} userId The discord user id that we are following.
-   * @param {object} [songInfo] If song info is provided, this will not be
-   * fetched first. If it is not, the information will be fetched from Spotify
-   * first.
-   * @param {boolean} [start=false] Should we setup the player with our settings
-   * because this is the first run?
-   */
-  function updateFollowingState(msg, userId, songInfo, start = false) {
-    checkMusic();
-    if (!start && !music.isSubjugated(msg)) {
-      endFollow(msg);
-      return;
-    }
-    if (!songInfo) {
-      getCurrentSong(userId, (err, song) => {
-        if (err) {
-          if (err == 'Nothing Playing') {
-            if (!following[msg.guild.id]) {
-              following[msg.guild.id] = {};
-            }
-            following[msg.guild.id].timeout =
-                self.client.setTimeout(function() {
-                  updateFollowingState(msg, userId, null, true);
-                }, 3000);
-          } else {
-            self.error(err);
-          }
-          return;
-        }
-        songInfo = song;
-        makeTimeout();
-      });
-    } else {
-      makeTimeout();
-    }
-
-    /**
-     * Start playing the music, and create a timeout to check the status, or for
-     * the next song.
-     *
-     * @private
-     */
-    function makeTimeout() {
-      if (!start && !music.isSubjugated(msg)) {
-        endFollow(msg);
-        return;
-      }
-      music.clearQueue(msg);
-      if (start) {
-        music.subjugate(msg);
-      }
-      if (songInfo && (start || songInfo.progress < 60000)) {
-        startMusic(msg, songInfo);
-      }
-      if (following[msg.guild.id] && following[msg.guild.id].timeout) {
-        self.client.clearTimeout(following[msg.guild.id].timeout);
-      }
-      following[msg.guild.id] = {
-        user: userId,
-        song: songInfo,
-        lastUpdate: Date.now(),
-      };
-
-      if (!songInfo || songInfo.duration) {
-        const delay = songInfo ? (songInfo.duration - songInfo.progress) : 3000;
-        following[msg.guild.id].timeout = self.client.setTimeout(function() {
-          updateFollowingState(msg, userId);
-        }, delay);
-      } else {
-        following[msg.guild.id].timeout = self.client.setTimeout(function() {
-          updateDuration(msg, userId);
-        }, 1000);
-      }
-    }
-  }
-
-  /**
-   * Fetch the song's length from music because Spotify was unable to provide it
-   * for us.
-   *
-   * @private
-   *
-   * @param {Discord~Message} msg The context.
-   * @param {string|number} userId The user id we are following.
-   */
-  function updateDuration(msg, userId) {
-    checkMusic();
-    if (following[msg.guild.id] && following[msg.guild.id].timeout) {
-      self.client.clearTimeout(following[msg.guild.id].timeout);
-    }
-    if (!music.isSubjugated(msg)) {
-      endFollow(msg);
-      return;
-    }
-    const dur = music.getDuration(msg);
-    const prog = music.getProgress(msg);
-    if (dur != null && prog != null) {
-      following[msg.guild.id].song.duration = dur * 1000;
-      const f = following[msg.guild.id];
-
-      const delay = f.song.duration - f.song.progress;
-      following[msg.guild.id].timeout = self.client.setTimeout(function() {
-        updateFollowingState(msg, userId);
-      }, delay);
-    } else {
-      following[msg.guild.id].timeout = self.client.setTimeout(function() {
-        updateDuration(msg, userId);
-      }, 1000);
-    }
-  }
-
-  /**
-   * Attempt to start playing the given song into a voice channel.
-   *
-   * @private
-   * @param {Discord~Message} msg Message that caused this to happen, and to
-   * pass into {@link Command} as context.
-   * @param {{name: string, artist: string, progress: number}} song The current
-   * song information. Name is song name, progress is progress into the song in
-   * milliseconds.
-   */
-  function startMusic(msg, song) {
-    checkMusic();
-    const seek = Math.round(song.progress / 1000 + (song.progress / 1000 / 5));
-    music.playSong(msg, song.name + ' by ' + song.artist, seek, true);
-  }
-
-  /**
-   * Update current reference to music submodule.
-   *
-   * @private
-   */
-  function checkMusic() {
-    if (!music || !music.initialized) {
-      music = self.bot.getSubmodule('./music.js');
-    }
-  }
-
-  /**
-   * Cleanup and delete data in order to stop following user.
-   *
-   * @private
-   * @param {Discord~Message} msg THe context to clear.
-   */
-  function endFollow(msg) {
-    if (following[msg.guild.id]) {
-      self.client.clearTimeout(following[msg.guild.id].timeout);
-    }
-    delete following[msg.guild.id];
-    checkMusic();
-    if (music) music.release(msg);
-  }
-}
-module.exports = new Spotify();
-
-//END OF SPOTIFY
-
-client.login(process.env.token);
+client.login(process.env.token); // Discord bot client auth.
