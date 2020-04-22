f95466d3d89a49d10487b690615b44ee9c14fa59
Gusvananda Imanullah <gusvananda@gmail.com>
2020-04-23 00:39:57 +0700

    Update mcbot.js

--- mcbot.js
+++ mcbot.js
@@ -361,4 +361,6 @@ client.setInterval(function(){
 client.login(process.env.token); // Discord bot client auth.
 nodeClient.login(na_HOST, na_KEY, (logged_in, err) => { // Pterodactyl API client auth.
     console.log(logged_in);
-	});
+	}).catch((error) => {
+		console.log(error.message);
+	}
