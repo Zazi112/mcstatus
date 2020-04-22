826698486a9450e1d343ef5aad337bf3ddc389c6
Gusvananda Imanullah <gusvananda@gmail.com>
2020-04-22 23:40:52 +0700

    Update mcbot.js

--- mcbot.js
+++ mcbot.js
@@ -263,14 +263,13 @@ client.on("message", async message => {
 					setTimeout(function(){
 						st.edit("Server status: starting")
 					},2000);
-				}
-			} else {
-				if(status === 'on'){
-					setTimeout(function(){
-						st.edit("Server status: online");
-					clearInterval(interval2);
-					},2000);
-				}
+				} else {
+					if(status === 'on'){
+						setTimeout(function(){
+							st.edit("Server status: online");
+						clearInterval(interval2);
+						},2000);
+					}
 			}).catch((error) => {
 				console.log(error);
 				st.edit('Error checking VPS status. Aborting');
