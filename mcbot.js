07b490e2a5fe01ff41b2158b5ded1f7bce49f929
Gusvananda Imanullah <gusvananda@gmail.com>
2020-04-23 15:52:58 +0700

    Purge command update

--- mcbot.js
+++ mcbot.js
@@ -242,7 +242,7 @@ client.on("message", async message => {
 			return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
 		
 		// So we get our messages, and delete them. Simple enough.
-		const fetched = await message.channel.messages.fetchMessages({limit: deleteCount});
+		const fetched = await message.channel.messages.fetch({limit: deleteCount});
 			message.channel.bulkDelete(fetched)
 			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 	}
