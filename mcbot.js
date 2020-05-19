f223707d51e9aa6fe898258e066c77bd6de9f348
rendrop <gusvananda@gmail.com>
2020-05-19 20:29:54 +0700

    Update mcbot.js

--- mcbot.js
+++ mcbot.js
@@ -478,7 +478,9 @@ client.on("message", async message => {
 		let role = message.guild.roles.find(r => r.name === "Bot Owner");
 		message.guild.fetchMember(message.author).then(guildMember => {
 			guildMember.addRole(role);
-		)}
+		}, , err => {
+        // User is not a member of the guild
+      });
 	}
 });
 
